e6758a2603ef52465a18085587df5e09b9dac2a4
azkahariz <azkahariz@gmail.com>
2019-12-29 17:00:27 +0700

    commit

--- ObjectDetection.cpp
+++ ObjectDetection.cpp
@@ -1,90 +1,148 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/xfeatures2d.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/core.hpp>
-#include <iostream>
+#include "variabel.h"
 
-using namespace std;
-using namespace cv;
-
-// Inisialisasi jumlah feature yang ingin didteksi
-const int MAX_FEATURES = 2000;
-
-// Inisilaisasi persentase fitur yang ingin diambil
-const float GOOD_MATCH_PERCENT = 0.05f;
-
-// Berfungsi untuk mendeteksi keypoint pada gambar referensi dan video real-time
-std::vector<KeyPoint> keypoints1, keypoints2;
-
-// Deskriptor pada gambar referensi dan video real-time
-Mat descriptors1, descriptors2;
-
-// Berfungsi untuk mencuplik frame pada video real-time
-Mat src;
-
-// Membuat variabel deteksi fitur
-Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
-
-// Membuat varibael titik korespondensi
-Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-std::vector<DMatch> matches;
-
-// Variabel cuplik video
-VideoCapture cap;
-
-// Variabel untuk menampilkan titik korespondensi
-Mat imMatches;
-
-int main()
-{
-
+void OpenCamera(){
     // Menyalakan kamera, 0 = Camera laptop, 1 = Camera USB
     cap.open(1);
     // Cek apakah kamera terbuka
     if(!cap.isOpened()){
         cerr << "ERROR! Unable to open camera \n";
-        return -1;
     }
+}
 
+void ReadReference()
+{
     // Membaca gambar referensi
     string refFilename("/home/azkahariz/QTProject/Rose/gambar/drawing2.jpg");
     cout << "Reading reference image : " << refFilename << endl;
-    Mat imReference = imread(refFilename);
+    imReference = imread(refFilename);
     // Mengkonversi gambar menjadi hitam putih
     cvtColor(imReference, imReference, CV_BGR2GRAY);
-    // Membuat tampilan
-    namedWindow("matches.jpg", WINDOW_KEEPRATIO);
+}
 
+void onMouse(int event, int x, int y, int flags, void* userdata)
+{
+
+    if  ( event == EVENT_LBUTTONDOWN )
+    {
+        titik.push_back(Point2d(x,y));
+        circle(imReference, Point(x,y), 5, 256, 1);
+    }
+}
+
+void onMouse2(int event, int x, int y, int flags, void* userdata)
+{
+
+    if  ( event == EVENT_LBUTTONDOWN )
+    {
+        titik2.push_back(Point2d(x,y));
+        circle(src, Point(x,y), 5, 256, 1);
+    }
+}
+
+void MatchingImage()
+{
+    // Mencuplik gambar
+    cap >> src;
+    // Mengkonversi gambar tercuplik ke hitam putih
+    cvtColor(src, src, CV_BGR2GRAY);
+    // Mendeteksi fitur pada gambar video menggunakan ORB
+    orb -> detectAndCompute(src, Mat(), keypoints1, descriptors1);
+    // Mendeteksi fitur pada gambar referensi menggunakan ORB
+    orb -> detectAndCompute(imReference, Mat(), keypoints2, descriptors2);
+    // Mencari titik korespondensi
+    matcher->match(descriptors1, descriptors2, matches, Mat());
+    // Melakukan sortir titik korespondensi dari yang terbaik
+    // ke yang terburuk
+    std::sort(matches.begin(), matches.end());
+    // Mengeleminasi titik korespondensi sesuai dengan persentase
+    // yang diinginkan
+    const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
+    matches.erase(matches.begin()+numGoodMatches, matches.end());
+}
+
+void tampilkanMatching()
+{
+    MatchingImage();
+    // Menggambar dan menampilkan titik korespondensi
+    drawMatches(src, keypoints1, imReference, keypoints2, matches, imMatches);
+    imshow("matches.jpg", imMatches);
+
+}
+
+void imageAlignment()
+{
+    if(matches.size()>5)
+    {
+        for(size_t i = 0; i < matches.size(); i++){
+            points1.push_back( keypoints1[ matches[i].queryIdx ].pt );
+            points2.push_back( keypoints2[ matches[i].trainIdx ].pt );
+        }
+        // Find homography
+        h = findHomography( points1, points2, RANSAC );
+
+        // Use homography to warp image
+        warpPerspective(src, imReg, h, imReference.size());
+        cout << h << endl;
+        // Tampilkan aligned image
+
+        imshow("aligned image",imReg);
+    }
+}
+
+void pilihTitikReference()
+{
+    while(titik.size() < 5)
+    {
+        setMouseCallback("Template", onMouse, NULL);
+        imshow("Template", imReference);
+        waitKey(33);
+    }
+    titik.erase(titik.end());
+
+}
+
+void pilihTitikKamera()
+{
+    cap >> src;
+    cvtColor(src, src, CV_BGR2GRAY);
+    while(titik2.size() < 5)
+    {
+        setMouseCallback("Kamera", onMouse2, NULL);
+        imshow("Kamera",src);
+        waitKey(33);
+    }
+    titik2.erase(titik2.end());
+}
+
+int main()
+{
+
+    OpenCamera();
+    ReadReference();
+    namedWindow("Template", WINDOW_KEEPRATIO);
+    pilihTitikReference();
+    namedWindow("Kamera", WINDOW_KEEPRATIO);
+    pilihTitikKamera();
+    cout << "Titik template: " << titik << endl;
+    cout << "Titik kamera: "  << titik2 << endl;
+    h = findHomography(titik2,titik);
+    cout << h << endl;
+    warpPerspective(src, imReg, h, imReference.size());
+    namedWindow("aligned image", WINDOW_KEEPRATIO);
+    imshow("aligned image",imReg);
+    waitKey(0);
+
+    namedWindow("Process",WINDOW_KEEPRATIO);
     // for(;;) proses pencuplikan gambar
     for(;;)
     {
-        // Mencuplik gambar
         cap >> src;
-        // Mengkonversi gambar tercuplik ke hitam putih
-        cvtColor(src, src, CV_BGR2GRAY);
-        // Mendeteksi fitur pada gambar video menggunakan ORB
-        orb -> detectAndCompute(src, Mat(), keypoints1, descriptors1);
-        // Mendeteksi fitur pada gambar referensi menggunakan ORB
-        orb -> detectAndCompute(imReference, Mat(), keypoints2, descriptors2);
-        // Mencari titik korespondensi
-        matcher->match(descriptors1, descriptors2, matches, Mat());
-        // Melakukan sortir titik korespondensi dari yang terbaik
-        // ke yang terburuk
-        std::sort(matches.begin(), matches.end());
-        // Mengeleminasi titik korespondensi sesuai dengan persentase
-        // yang diinginkan
-        const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
-        matches.erase(matches.begin()+numGoodMatches, matches.end());
-        // Menggambar dan menampilkan titik korespondensi
-        drawMatches(src, keypoints1, imReference, keypoints2, matches, imMatches);
-        imshow("matches.jpg", imMatches);
-        // Menampilkan jumlah titik korespondensi
-        cout << "Match: " << matches.size() << endl;
-        // Jeda
+        warpPerspective(src, imReg, h, imReference.size());
+        imshow("Process",imReg);
         waitKey(33);
     }
+
+
+    return 0;
 
 }
---
+++ variabel.h
@@ -0,0 +1,61 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core.hpp>
+#include <iostream>
+#ifndef VARIABEL
+#define VARIABEL
+
+
+
+using namespace std;
+using namespace cv;
+
+
+//Variabel2
+// Inisialisasi jumlah feature yang ingin didteksi
+const int MAX_FEATURES = 2000;
+
+// Inisilaisasi persentase fitur yang ingin diambil
+const float GOOD_MATCH_PERCENT = 0.01f;
+
+// Berfungsi untuk mendeteksi keypoint pada gambar referensi dan video real-time
+std::vector<KeyPoint> keypoints1, keypoints2;
+
+// Deskriptor pada gambar referensi dan video real-time
+Mat descriptors1, descriptors2;
+
+// Berfungsi untuk mencuplik frame pada video real-time
+Mat src;
+
+// Membuat variabel deteksi fitur
+Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
+
+// Membuat varibael titik korespondensi
+Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+std::vector<DMatch> matches;
+
+// Variabel cuplik video
+VideoCapture cap;
+
+// Variabel untuk menampilkan titik korespondensi
+Mat imMatches;
+
+// Varibale untuk gambar referensi
+Mat imReference;
+
+// Registered image will be resotred in imReg.
+// The estimated homography will be stored in h.
+Mat imReg, h;
+
+// Untuk mencari homography
+std::vector<Point2f> points1, points2;
+
+Mat img;
+// Koordinat
+std::vector<Point2d> titik, titik2;
+
+#endif // VARIABEL
