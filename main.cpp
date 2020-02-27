58040a9113ae6927f0d9a989fd577faa7aeefaa1
azkahariz <azkahariz@gmail.com>
2020-02-27 20:06:37 +0700

    Perubahan cukup banyak

--- main.cpp
+++ main.cpp
@@ -15,11 +15,12 @@ void OpenCamera(){
 void ReadReference()
 {
     // Membaca gambar referensi
-    string refFilename("/media/azkahariz/Local Disk1/Proyek/Bomb Scoring/Rose/gambar/Reference_2.jpg");
-    cout << "Reading reference image : " << refFilename << endl;
-    imReference = imread(refFilename);
+    cap >> imReference;
+    //string refFilename("/media/azkahariz/Local Disk1/Proyek/Bomb Scoring/Rose/gambar/Reference_2.jpg");
+    //cout << "Reading reference image : " << refFilename << endl;
+    //imReference = imread(refFilename);
     // Mengkonversi gambar referensi menjadi hitam putih
-    cvtColor(imReference, imReference, CV_BGR2GRAY);
+    //cvtColor(imReference, imReference, CV_BGR2GRAY);
 }
 
 void onMouse(int event, int x, int y, int flags, void* userdata)
@@ -69,8 +70,14 @@ void pilihTitikKamera()
 
 void BackgroundSubtraction()
 {
-    GaussianBlur(imReg,frame,Size(11,11),0);
+    //GaussianBlur(imReg,frame,Size(11,11),0);
+    GaussianBlur(src,frame,Size(11,11),0);
     pBackSub->apply(frame,fgMask);
+    morphologyEx(fgMask,fgMask,MORPH_OPEN,element);
+    dilate(fgMask, dilatasi, Mat::ones(20,20,CV_8U));
+    findContours(dilatasi, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+    vector<Rect> boundRect( contours.size() );
+
 }
 
 int main()
@@ -80,19 +87,19 @@ int main()
     ReadReference();
     namedWindow("Template", WINDOW_KEEPRATIO);
     pilihTitikReference();
-    namedWindow("Kamera", WINDOW_KEEPRATIO);
-    pilihTitikKamera();
+    //namedWindow("Kamera", WINDOW_KEEPRATIO);
+    //pilihTitikKamera();
     cout << "Titik template: " << titik << endl;
-    cout << "Titik kamera: "  << titik2 << endl;
+    //cout << "Titik kamera: "  << titik2 << endl;
 
-    h = findHomography(titik2,titik);
-    cout << "h :" << h << endl;
+    //h = findHomography(titik2,titik);
+    //cout << "h :" << h << endl;
 
-    warpPerspective(src, imReg, h, imReference.size());
+    //warpPerspective(src, imReg, h, imReference.size());
 
-    namedWindow("aligned image", WINDOW_KEEPRATIO);
-    imshow("aligned image",imReg);
-    waitKey(0);
+    //namedWindow("aligned image", WINDOW_KEEPRATIO);
+    //imshow("aligned image",imReg);
+    //waitKey(0);
 
     namedWindow("Process",WINDOW_KEEPRATIO);
     namedWindow("FG Mask",WINDOW_KEEPRATIO);
@@ -101,11 +108,29 @@ int main()
     {
 
         cap >> src;
-        warpPerspective(src, imReg, h, imReference.size());
+        //warpPerspective(src, imReg, h, imReference.size());
 
-        cvtColor(imReg, imRegGray,CV_BGR2GRAY);
+        //cvtColor(imReg, imRegGray,CV_BGR2GRAY);
+        cvtColor(src, imRegGray, CV_BGR2GRAY);
         medianBlur(imRegGray, imRegGray, 3);
         BackgroundSubtraction();
+        cout << "Bomb terdeteksi : " << contours.size() << endl;
+        vector<Rect> boundRect( contours.size() );
+        if (contours.size() > 0)
+        {
+            for(int i = 0; i < contours.size(); i++)
+            {
+                double area = contourArea(contours[i]);
+                cout << "area contours-" << i << ": " << area << endl;
+                if(area > 2000)
+                {
+                   boundRect[i] = boundingRect(contours[i]);
+                   rectangle( src, boundRect[i].tl(), boundRect[i].br(), (0,0,255),2 );
+                }
+                //boundRect[i] = boundingRect( Mat(contours[i]) )
+                //cout << "area : " << area << endl;
+            }
+        }
 
         //        // Circles target
         //        HoughCircles(imRegGray,circlesTarget,HOUGH_GRADIENT, 1, imRegGray.rows/8, 20, 20, 280, 300);
@@ -146,7 +171,8 @@ int main()
         //            circle(imReg, centerBomb, radiusBomb, Scalar(255, 0, 0), 2, 8, 0);
         //        }
 
-        imshow("Process",imReg);
+        //imshow("Process",imReg);
+        imshow("Process",src);
         imshow("FG Mask", fgMask);
         waitKey(33);
     }
